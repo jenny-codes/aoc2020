2bfab245a0a021abfe84cf10edaee2c98ac670e3
jennyshih <jinghua.shih@gmail.com>
2020-12-04 09:02:06 +0800

    [Chore] Reformat file structure

--- days/day2.exs
+++ days/day2.exs
@@ -1,27 +1,28 @@
 defmodule Day2 do
   def run do
-    formatted_input = "daily/inputs/day2.txt"
+    formatted_input =
+      "daily/inputs/day2.txt"
       |> File.stream!()
       |> Stream.map(&String.trim/1)
       |> Stream.map(&parse/1)
 
     formatted_input
-      |> Stream.filter(&validate1/1)
-      |> Enum.count
-      |> IO.inspect(label: "result1")
+    |> Stream.filter(&validate1/1)
+    |> Enum.count()
+    |> IO.inspect(label: "result1")
 
     formatted_input
-      |> Stream.filter(&validate2/1)
-      |> Enum.count
-      |> IO.inspect(label: "result2")
+    |> Stream.filter(&validate2/1)
+    |> Enum.count()
+    |> IO.inspect(label: "result2")
   end
 
   defp parse(raw_str) do
     # Format: "1-4 j: jjjqzmgbjwpj"
     normalize = fn %{"first_int" => first, "second_int" => second} = item ->
       item
-      |> Map.put("first_int", first |> String.to_integer)
-      |> Map.put("second_int", second |> String.to_integer)
+      |> Map.put("first_int", first |> String.to_integer())
+      |> Map.put("second_int", second |> String.to_integer())
     end
 
     ~r/(?<first_int>\d+)-(?<second_int>\d+) (?<char>\w): (?<text>\w*)$/
@@ -31,10 +32,12 @@ defmodule Day2 do
 
   defp validate1(%{"first_int" => min, "second_int" => max, "char" => char, "text" => text}) do
     valid_range = min..max
-    char_count = text
-    |> String.split(char)
-    |> Enum.count
-    |> fn x -> x - 1 end.()
+
+    char_count =
+      text
+      |> String.split(char)
+      |> Enum.count()
+      |> (fn x -> x - 1 end).()
 
     char_count in valid_range
   end
