ac4c7bec89d052e99add26752505909664492712
jennyshih <jinghua.shih@gmail.com>
2020-12-19 20:03:14 +0800

    [Feat] Implement Chinese Remainder Theorem and Extended Euclidean algorithm

---
+++ lib/math_util.ex
@@ -0,0 +1,71 @@
+defmodule MathUtil do
+  # ===========================================
+  # Greatest common divisor
+  # ===========================================
+  def gcd(a, 0), do: a
+  def gcd(0, b), do: b
+  def gcd(a, b), do: gcd(b, rem(a, b))
+
+  def gcd(list) do
+    Enum.reduce(list, 0, fn x, acc ->
+      gcd(acc, x)
+    end)
+  end
+
+  # ===========================================
+  # Least common multiple
+  # ===========================================
+  def lcm(0, 0), do: 0
+  def lcm(a, b), do: round(a * b / gcd(a, b))
+
+  def lcm(list) do
+    Enum.reduce(list, 1, fn x, acc ->
+      lcm(acc, x)
+    end)
+  end
+
+  # ===========================================
+  # Extended Euclidean algorithm
+  # ===========================================
+  def ext_euclid(a, b) do
+    ext_euclid(a, b, [1, 0], [0, 1])
+  end
+
+  def ext_euclid(a, 0, [ma, _], [mb, _]), do: {a, ma, mb}
+
+  def ext_euclid(a, b, [ma1, ma2], [mb1, mb2]) do
+    new_ma = ma1 - div(a, b) * ma2
+    new_mb = mb1 - div(a, b) * mb2
+
+    ext_euclid(b, rem(a, b), [ma2, new_ma], [mb2, new_mb])
+  end
+
+  # ===========================================
+  # Chinese Remainder Theorem
+  # ===========================================
+  @spec crt([{integer(), integer()}]) :: integer()
+  def crt(input) do
+    {t, coef} =
+      Enum.reduce(input, fn {y_const, y_coef}, {x_const, x_coef} ->
+        run_crt_pair(x_const, x_coef, y_const, y_coef)
+      end)
+
+    add_until_positive(t, coef)
+  end
+
+  defp run_crt_pair(x_const, x_coef, y_const, y_coef) do
+    {_, x_bezout_identity, y_bezout_identity} = ext_euclid(x_coef, y_coef)
+    new_coef = x_coef * y_coef
+    new_const = y_const * x_bezout_identity * x_coef + x_const * y_bezout_identity * y_coef
+
+    {new_const, new_coef}
+  end
+
+  defp add_until_positive(const, coef) do
+    if const > 0 do
+      const
+    else
+      (Integer.floor_div(abs(const), coef) + 1) * coef + const
+    end
+  end
+end
---
+++ test/math_util_test.exs
@@ -0,0 +1,45 @@
+defmodule MathUtilTest do
+  use ExUnit.Case
+
+    describe "gcd" do
+    test "works with a list" do
+      assert MathUtil.gcd([1, 3]) == 1
+      assert MathUtil.gcd([2, 3]) == 1
+      assert MathUtil.gcd([2, 4]) == 2
+      assert MathUtil.gcd([2, 4, 6]) == 2
+      assert MathUtil.gcd([2, 4, 6, 12]) == 2
+    end
+
+    test "returns value when the other input is 0" do
+      assert MathUtil.gcd(3, 0) == 3
+      assert MathUtil.gcd(0, 3) == 3
+    end
+  end
+
+  describe "lcm" do
+    test "works with a list" do
+      assert MathUtil.lcm([1, 3]) == 3
+      assert MathUtil.lcm([2, 3]) == 6
+      assert MathUtil.lcm([2, 4]) == 4
+      assert MathUtil.lcm([6, 4]) == 12
+      assert MathUtil.lcm([3, 4, 6]) == 12
+      assert MathUtil.lcm([3, 4, 5, 6]) == 60
+    end
+  end
+
+  describe "ext_euclid" do
+    test "works" do
+      assert MathUtil.ext_euclid(240, 46) == {2, -9, 47}
+      assert MathUtil.ext_euclid(3, 5) == {1, 2, -1}
+      assert MathUtil.ext_euclid(5369, 31) == {1, -5, 866}
+    end
+  end
+
+  describe "crt" do
+    test "works" do
+      assert MathUtil.crt([{0, 3}, {3, 4}, {4, 5}]) == 39
+      assert MathUtil.crt([{2, 3}, {3, 5}, {2, 7}]) == 23
+      assert MathUtil.crt([{2, 3}, {3, 5}, {2, 7}, {1, 11}, {10, 13}]) == 23
+    end
+  end
+end
